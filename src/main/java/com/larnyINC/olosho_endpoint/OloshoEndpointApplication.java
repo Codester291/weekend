2033c56231bb60828562f9d60ed956b9d4fc7beb
Rextless OX <codester@github.com>
2019-03-08 13:43:14 +0000

    welcome

---
+++ src/main/java/com/larnyINC/olosho_endpoint/OloshoEndpointApplication.java
@@ -0,0 +1,13 @@
+package com.larnyINC.olosho_endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OloshoEndpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OloshoEndpointApplication.class, args);
+	}
+
+}
